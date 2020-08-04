6fa1b8b10763aa472cbf542e0ae914f67bcbd724
casstey <68892174+casstey@users.noreply.github.com>
2020-08-04 20:38:42 +0300

    ok

--- index.js
+++ index.js
@@ -1,56 +1,67 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-client.disabledMembers = new Map();
-client.login("NzQwMTIxOTkzODk0MTY2NTc4.XykaUg.NFip---zzDVB1_-vITC_ymLvhR0");
+const fs = require("fs")
+const discord = require("discord.js")
+const prefix = "!"
+const enmap = require("enmap")
+const client = new discord.Client()
+client.commands = new discord.Collection()
 
-client.on('ready', async () => {
-        console.log('I am ready!');
-        client.user.setPresence({
-            activity: {
-            name:  "I want to talk to your manager.",
-            type: "PLAYING"
-          
-            }
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('js'))
 
-    });
+client.aliases = new discord.Collection()
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+  command.aliases.forEach(alias => client.aliases.set(alias, command.name))
+  client.commands.set(command.name, command)
+}
 
+client.reactionroles = new enmap({name:"reactionroles"})
 
-client.on('message', async (message) => {
-        if(message.author.bot) return;
-        const args = message.content.trim().split(/ +/g);
-        if((args[0].toLowerCase() == 'i\'m' || args[0].toLowerCase() == 'im') && (args[1]) && !client.disabledMembers.has(message.author.id)){
-                message.channel.send(`Hi ${args.slice(1).join(' ')}, I want to talk to your manager.`);
-        }
-        owos = ['owo', 'ovo', 'uwu', 'uvu', 'unu', 'ono', 'umu', 'ene', 'nun'];
-        if(owos.includes(args[0].toLowerCase()) && !client.disabledMembers.has(message.author.id)) message.channel.send('stfu');
-        if(message.mentions.has(client.user)){
-    
-                if(message.content.indexOf('stop') !== -1) client.disabledMembers.set(message.author.id, message.guild.id)
-                else if (message.content.indexOf('no, get your fakeass manager here!') !== -1 && client.disabledMembers.has(message.author.id)) client.disabledMembers.delete(message.author.id);
-        }
- 
-        managers = ['cashier', 'worker', 'manager'];
-        if(managers.includes(args[0].toLowerCase()) && !client.disabledMembers.has(message.author.id)) message.channel.send('I SAID, GET ME YOUR MANAGER!');
-        if(message.mentions.has(client.user)){
-                
-            if(message.content.indexOf('I wanna talk to your manager.') !== -1) client.disabledMembers.set(message.author.id, message.guild.id)
-                else if (message.content.indexOf('stfu') !== -1 && client.disabledMembers.has(message.author.id)) client.disabledMembers.delete(message.author.id);
-        }
-
-        shuts = ['shut', 'stfu', 'shut the fuck up'];
-        if(shuts.includes(args[0].toLowerCase()) && !client.disabledMembers.has(message.author.id)) message.channel.send("Listen here, I will not tolerate you saying the words that consist of the letters 's h u t  u p'. I only want to talk to your manager, so take your own advice and close your annoying fucking mouth and get your fakeass manager here.");
-        if(message.mentions.has(client.user)){
-
-        }
-        stops = ['stop', 'enough', 'annoying'];
-        if(stops.includes(args[0].toLowerCase()) && !client.disabledMembers.has(message.author.id)) message.channel.send("Listen here you little shit, you invited me to this server so shut up, and get me your manager.");
-        if(message.mentions.has(client.user)){
-
-        }
-        managers = ['developer', 'owner'];
-        if(managers.includes(args[0].toLowerCase()) && !client.disabledMembers.has(message.author.id)) message.channel.send("This bot is made by; Casstey Schwarzenegger#0001 ");
-        if(message.mentions.has(client.user)){
-        }
-    });
-
+client.snipes = new Map()
+client.on('messageDelete', function(message, channel){
+  
+  client.snipes.set(message.channel.id, {
+    content:message.content,
+    author:message.author.tag,
+    image:message.attachments.first() ? message.attachments.first().proxyURL : null
+  })
+  
 })
+
+client.on("ready", () => {
+  console.log("Im online")
+  client.user.setPresence({
+    activity: {
+    name: client.users.cache.size + "  members | !help for commands!",
+    type: "WATCHING"
+  
+    }
+  
+  });
+  
+  });
+
+client.on('message', message => {
+  if(message.mentions.users.first()===client.user){
+    message.channel.send("Hi there, The prefix is '!' ")
+
+};
+});
+ 
+
+client.on("message" , async message => {
+  if(message.author.bot) return
+  
+  if(!message.content.startsWith(prefix)) return 
+  
+  if(!message.member) message.member = await message.guild.fetchMember(message);
+  const args = message.content.slice(prefix.length).split(/ +/)
+  const command = args.shift().toLowerCase()
+  const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command))
+  
+  if(cmd === null ) return
+  
+  if(cmd) cmd.run(client, message, args)
+  if(!cmd) return
+})
+
+client.login(process.env.token) 
